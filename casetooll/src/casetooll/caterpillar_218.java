7022527e44fabdc594630217822bd0d4b2a1d057
nikita cardozo <nikitacardozo14@gmail.com>
2024-02-10 09:46:09 +0530

    commit1

--- casetooll/src/casetooll/caterpillar_218.java
+++ casetooll/src/casetooll/caterpillar_218.java
@@ -2,8 +2,18 @@ package casetooll;
 
 public class caterpillar_218 {
 
+	int moves,cabbages;
+	
+	
+    caterpillar_218(int moves, int cabbages){
+    	
+    	this.moves=moves;
+    	this.cabbages=cabbages;
+    }
 	
 	
 	
 	
-}
+}	
+	
+	
